4e1be8b7057996056821ba4291c99e33098e3245
alexbarlow214 <alexbarlow214@gmail.com>
2023-01-28 21:39:45 -0500

    styled type column

--- src/scenes/workouts/index.jsx
+++ src/scenes/workouts/index.jsx
@@ -1,62 +1,93 @@
-import { DataGrid }from "@mui/x-data-grid"
-import { tokens } from "../../theme"
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
 import Exercises from "../../Exercises";
-import { Box, useTheme } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 import { useState, useEffect, inputRef } from "react";
 import Header from "../../components/Header";
-
-
-
+import { mock } from "./mock";
+import FitnessCenterOutlinedIcon from '@mui/icons-material/FitnessCenterOutlined';
 
 export default function Workouts() {
-    const theme = useTheme()
-    const colors = tokens(theme.palette.mode)
-    const columns = 
-    [{ field: "name", headerName: "Name", flex: 1, cellClassName: "name-column-cell"}, 
-    {field: "type", headerName: "Type", type: "number", headerAlign: "left", align: "left"}]
-    async function handleInput(e){
-        const name = inputRef.current.value
-        await fetch("http://localhost:8002/exercise", {
-        method: 'POST',
-        headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const columns = [
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column-cell",
+    },
+    {
+      field: "type",
+      headerName: "Type",
+      headerAlign: "left",
+      flex: 1,
+      align: "left",
+      renderCell: ({ row: { type } }) => {
+        return (
+          <Box
+            width="60%"
+            m="0 auto"
+            p="5px"
+            display="flex"
+            justifyContent="center"
+          >
+            <FitnessCenterOutlinedIcon sx={{mr: "10px"}}/>
+            {type === "Chest" && <Typography color={colors.greenAccent[400]} >{"Chest"}</Typography>}
+            {type === "Legs" && <Typography color={colors.greenAccent[700]} >{"Legs"}</Typography>}
+            {type === "Bicep" && <Typography color={colors.blueAccent[400]} >{"Bicep"}</Typography>}
+            {type === "Tricep" && <Typography color={colors.greenAccent[700]} >{"Tricep"}</Typography>}
+            {type === "Back" && <Typography color={colors.redAccent[700]} >{"Back"}</Typography>}
+            {type === "Sholder" && <Typography color={colors.redAccent[400]} >{"Sholder"}</Typography>}
+          </Box>
+        );
+      },
+    },
+    {
+      field: "PR",
+      headerName: "PR",
+      headerAlign: "left",
+      flex: 1,
+      align: "left",
+    },
+  ];
+  async function handleInput(e) {
+    const name = inputRef.current.value;
+    await fetch("http://localhost:8002/exercise", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         name: name,
         type: 0,
-      })
-    })
-        
-        setRetrieve(oldEx => {
-          return !oldEx
-        })
-        console.log(shouldRetrieve)
-        console.log(name)
-    
-      }
+      }),
+    });
 
-      const[shouldRetrieve, setRetrieve] = useState(true)
+    setRetrieve((oldEx) => {
+      return !oldEx;
+    });
+    console.log(shouldRetrieve);
+    console.log(name);
+  }
 
-  const [exercises, setExercises] = useState([])
+  const [shouldRetrieve, setRetrieve] = useState(true);
+
+  const [exercises, setExercises] = useState([]);
   useEffect(() => {
     fetch("http://localhost:8002/exercise")
       .then((res) => res.json())
       .then((data) => setExercises(data.data));
   }, [shouldRetrieve]);
 
-    return (
+  return (
     <Box m="20px">
-        <Header title="TEAM" subtitle="Managing the Team Members"/>
-        <Box
-        m="40px 0 0 0" height = "75vh">
-          {console.log(exercises)}
-            <DataGrid
-                rows={exercises}
-                columns={columns}
-            />
-        </Box>
-
+      <Header title="TEAM" subtitle="Managing the Team Members" />
+      <Box m="40px 0 0 0" height="75vh">
+        {console.log(exercises)}
+        <DataGrid rows={mock} columns={columns} />
+      </Box>
     </Box>
-  )
+  );
 }
---
+++ src/scenes/workouts/mock.js
@@ -0,0 +1,69 @@
+export const mock = [
+    {
+        id: 1,
+        name: "Barbell Bench Press",
+        type: "Chest",
+        PR: "125X3"
+    },
+    {
+        id: 2,
+        name: "Dumbell Inclined Press",
+        type: "Chest",
+        PR: "40X9"
+    },
+    {
+        id: 3,
+        name: "Chest Fly",
+        type: "Chest",
+        PR: "110X10"
+    },
+    {
+        id: 4,
+        name: "Barbell Squat",
+        type: "Legs",
+        PR: "115X15"
+    },
+    {
+        id: 5,
+        name: "Leg Press",
+        type: "Legs",
+        PR: "200X17"
+    },
+    {
+        id: 6,
+        name: "Lat Pull Down",
+        type: "Back",
+        PR: "100X9"
+    },
+    {
+        id: 7,
+        name: "Horinzontal Row",
+        type: "Back",
+        PR: "110X9"
+    },
+    {
+        id: 8,
+        name: "Bicep Curl",
+        type: "Bicep",
+        PR: "50X8"
+    },
+    {
+        id: 9,
+        name: "Tricep Push-Down",
+        type: "Tricep",
+        PR: "60X10"
+    },
+    {
+        id: 10,
+        name: "Dumbell Sholder Press",
+        type: "Sholder",
+        PR: "30X7"
+    },
+    {
+        id: 11,
+        name: "Latteral Raise",
+        type: "Sholder",
+        PR: "15X9"
+    },
+
+]
